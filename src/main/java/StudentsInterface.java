136d884a70e3a64a6e1965aa9210e34de786f70c
Gustavo Rodrigues <163344139+gustavo-rdrgs@users.noreply.github.com>
2024-04-15 19:38:05 -0300

    Update StudentsInterface.java
    adicionado novos métodos

--- src/main/java/StudentsInterface.java
+++ src/main/java/StudentsInterface.java
@@ -1,5 +1,8 @@
 public interface StudentsInterface {
     public boolean cadastrarAluno(Students s);
     public boolean cadastrarCadeira();
+    public Students pesquisarAluno(String matricula);
+    public boolean listarAlunosDoCurso(String curso);
+    public double calcularCra(String matricula, Cadeira cadeira);
 
 }
