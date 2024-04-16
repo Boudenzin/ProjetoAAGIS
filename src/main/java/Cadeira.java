4e191742c825f09572c8d91a5a5541dcc0197ad5
Romildo Júnior <romildo-rrj@hotmail.com>
2024-04-16 20:14:16 -0300

    Criação do Construtor

--- src/main/java/Cadeira.java
+++ src/main/java/Cadeira.java
@@ -4,4 +4,12 @@ public class Cadeira {
     private double nota1;
     private double nota2;
     private double nota3;
+
+    public Cadeira(String nome, String professor) {
+        this.nome = nome;
+        this.professor = professor;
+        this.nota1 = 0.0;
+        this.nota2 = 0.0;
+        this.nota3 = 0.0;
+    }
 }
