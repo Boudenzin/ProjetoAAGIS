665a099342b5aef5cdc60398a2f0b361bcc80266
Romildo Júnior <romildo-rrj@hotmail.com>
2024-04-22 15:14:11 -0300

    Alterações de métodos desnecessários e criação de outros

--- src/main/java/TurmaInterface.java
+++ src/main/java/TurmaInterface.java
@@ -1,8 +1,15 @@
-public interface AlunoInterface {
-    public boolean cadastrarAluno(Aluno s);
-    public boolean cadastrarCadeira(Cadeira cadeira);
-    public Aluno pesquisarAluno(String matricula);
-    public boolean listarAlunosDoCurso(String curso);
-    public double calcularCra(String matricula, Cadeira cadeira);
+import java.util.List;
+public interface TurmaInterface {
+    public void cadastrarNovaTurma(Turma turma) throws TurmaJaCriadaException;
 
+    public void cadastrarAlunoNaTurma(Aluno aluno, String nomeTurma);
+
+   /* public Aluno buscarAlunoPorMatricula(String matricula, String nomeTurma);
+
+    public boolean verificarExistenciaAluno(String matricula, String nomeTurma);
+
+    public List<Aluno> listarAlunosDaTurma(String nomeTurma);
+
+
+    */
 }
