70279d39a4b0e946b0d8ca3c70c77019de0b4552
Romildo Júnior <romildo-rrj@hotmail.com>
2024-04-22 15:13:21 -0300

    Mudança de Classe Identidade

--- src/main/java/Turma.java
+++ src/main/java/Turma.java
@@ -1,15 +1,54 @@
+import java.util.List;
+import java.util.ArrayList;
 public class Turma {
-    private String nome;
-    private String professor;
-    private double nota1;
-    private double nota2;
-    private double nota3;
 
-    public Turma(String nome, String professor) {
+    private String nome;
+    private String docente;
+    private List<Aluno> alunos;
+
+    public Turma(String nome, String docente) {
         this.nome = nome;
-        this.professor = professor;
-        this.nota1 = 0.0;
-        this.nota2 = 0.0;
-        this.nota3 = 0.0;
+        this.alunos = new ArrayList<>();
+        this.docente = docente;
     }
+
+    public void addAluno(Aluno aluno) {
+        if (!alunos.contains(aluno)) {
+            alunos.add(aluno);
+            System.out.println("Aluno cadastrado com sucesso");
+        } else {
+            System.out.println("Aluno " + aluno.getNome() + " já está na turma " + nome);
+        }
+    }
+
+    public void removeAluno(Aluno aluno) {
+        if (alunos.contains(aluno)) {
+            alunos.remove(aluno);
+        } else {
+            System.out.println("Aluno " + aluno.getNome() + " não está na turma " + nome);
+        }
+    }
+
+    public Aluno getAluno(String nome) throws AlunoNaoEncontradoException{
+        for (Aluno aluno : alunos) {
+            if (aluno.getNome().equals(nome)) {
+                return aluno;
+            }
+        }
+        throw new AlunoNaoEncontradoException("Aluno " + nome + " não encontrado na turma " + this.nome);
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
 }
+
