3b002a23bea90401ac7395bbc8674393955d6e07
Romildo Rodrigues <139826286+Boudenzin@users.noreply.github.com>
2024-04-24 08:23:14 -0300

    Criação do Menu em uma String
    Falta fazer o JOptionPane

--- src/main/java/AagisProgram.java
+++ src/main/java/AagisProgram.java
@@ -1,5 +1,16 @@
 public class AagisProgram {
     public static void main(String[] args) {
 
+        String = """
+            1. Cadastrar Turma
+            2. Cadastrar Aluno Na Turma
+            3. Ver Todos Os Alunos Da Turma
+            4. Remover Aluno Da Turma
+            5. Alterar Nota do Aluno da Turma
+            6. Pesquisar Aluno Por Matrícula
+            7. Sair
+
+            """;
+
     }
 }
