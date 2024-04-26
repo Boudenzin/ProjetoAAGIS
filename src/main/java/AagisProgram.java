f194f34d647ccd208105d2489c496345ec0f6e97
gustavo-rdrgs <gustavorsantos2713@gmail.com>
2024-04-26 15:10:31 -0300

    implementação no main do sistema de recuperar e gravar turmas

--- src/main/java/AagisProgram.java
+++ src/main/java/AagisProgram.java
@@ -5,28 +5,40 @@ import java.util.List;
 public class AagisProgram {
     public static void main(String[] args) {
         TurmaList sistemaTurmas = new TurmaList();
+        try {
+            sistemaTurmas.recuperaDadosEmTurma();
+            JOptionPane.showMessageDialog(null, "Dados de Turmas recuperados com sucesso.");
+        } catch (IOException e){
+            JOptionPane.showMessageDialog(null, "Dados de Turmas não puderam ser recuperados. Sistema iniciando sem nenhuma Turma.");
+        }
+
         String menu = """
             1. Cadastrar Turma
             2. Cadastrar Aluno Na Turma
             3. Ver Todos Os Alunos Da Turma
             4. Remover Aluno Da Turma
-            5. Alterar Nota do Aluno da Turma
-            6. Pesquisar Aluno Por Matrícula
+            5. Pesquisar Aluno Por Matrícula
+            6. Remover Turma
             7. Sair
 
             """;
 
         boolean continuar = true;
         while (continuar){
-            String opcao = JOptionPane.showInputDialog(null, menu);
+            String nomeTurma;
+            String nomeDocente;
+            String nomeAluno;
+            String matricula;
+            String curso;
 
+            String opcao = JOptionPane.showInputDialog(null, menu);
             switch (opcao) {
 
                 case "1":
                     try {
-                        String nomeDaTurma = JOptionPane.showInputDialog(null, "Digite o nome da turma: ");
-                        String nomeDocente = JOptionPane.showInputDialog(null, "Digite o nome do docente: ");
-                        Turma novaTurma = new Turma(nomeDaTurma, nomeDocente);
+                        nomeTurma = JOptionPane.showInputDialog("Digite o nome da turma: ");
+                        nomeDocente = JOptionPane.showInputDialog("Digite o nome do docente: ");
+                        Turma novaTurma = new Turma(nomeTurma, nomeDocente);
                         sistemaTurmas.cadastrarNovaTurma(novaTurma);
                         JOptionPane.showMessageDialog(null, "Turma cadastrada com sucesso.");
                     } catch (TurmaJaCriadaException e) {
@@ -36,10 +48,10 @@ public class AagisProgram {
 
                 case "2":
                     try {
-                        String nomeTurma = JOptionPane.showInputDialog(null, "Digite o nome da turma que deseja cadastrar o aluno(a)");
-                        String nomeAluno = JOptionPane.showInputDialog(null, "Digite o nome do(a) aluno(a)");
-                        String matricula = JOptionPane.showInputDialog(null, "Digite a matricula do aluno(a)");
-                        String curso = JOptionPane.showInputDialog(null, "Digite o curso no qual o(a) aluno(a) está cursando");
+                        nomeTurma = JOptionPane.showInputDialog("Digite o nome da turma que deseja cadastrar o(a) aluno(a)");
+                        nomeAluno = JOptionPane.showInputDialog("Digite o nome do(a) aluno(a)");
+                        matricula = JOptionPane.showInputDialog("Digite a matricula do(a) aluno(a)");
+                        curso = JOptionPane.showInputDialog("Digite o curso que o(a) aluno(a) está cursando");
                         Aluno novoAluno = new Aluno(nomeAluno, matricula, curso);
                         sistemaTurmas.cadastrarAlunoNaTurma(novoAluno, nomeTurma);
                         JOptionPane.showMessageDialog(null, "Aluno cadastrado com sucesso.");
@@ -50,10 +62,10 @@ public class AagisProgram {
 
                 case "3":
                     try {
-                        String turma = JOptionPane.showInputDialog(null, "Digite o nome da turma");
-                        List<Aluno> alunosDaTurma = sistemaTurmas.listarAlunosDaTurma(turma);
+                        nomeTurma = JOptionPane.showInputDialog("Digite o nome da turma");
+                        List<Aluno> alunosDaTurma = sistemaTurmas.listarAlunosDaTurma(nomeTurma);
                         if (alunosDaTurma.isEmpty()){
-                            JOptionPane.showMessageDialog(null, "Não há alunos cadastrados na turma '"+ turma +"'");
+                            JOptionPane.showMessageDialog(null, "Não há alunos cadastrados na turma '"+ nomeTurma +"'");
                         } else {
                             StringBuilder saida = new StringBuilder();
                             for (Aluno a: alunosDaTurma){
@@ -61,7 +73,6 @@ public class AagisProgram {
                             }
                             JOptionPane.showMessageDialog(null, saida.toString());
                         }
-
                     } catch (TurmaNaoEncontradaException e){
                         JOptionPane.showMessageDialog(null, e.getMessage());
                     }
@@ -69,53 +80,44 @@ public class AagisProgram {
 
                 case "4":
                     try {
-                        String nomeTurma1 = JOptionPane.showInputDialog(null, "Digite o nome da turma");
-                        String mAluno = JOptionPane.showInputDialog(null, "Digite o matricula do(a) aluno(a) que deseja remover");
-                        sistemaTurmas.removerAlunoDaTurma(mAluno, nomeTurma1);
+                        nomeTurma = JOptionPane.showInputDialog("Digite o nome da turma");
+                        matricula = JOptionPane.showInputDialog("Digite o matricula do(a) aluno(a) que deseja remover");
+                        sistemaTurmas.removerAlunoDaTurma(matricula, nomeTurma);
+                        JOptionPane.showMessageDialog(null, "Aluno removido com sucesso");
                     } catch (TurmaNaoEncontradaException | AlunoNaoEncontradoException e) {
                         JOptionPane.showMessageDialog(null, e.getMessage());
                     }
                     break;
 
                 case "5":
-                    boolean inputValido = false;
-                    double n1;
-                    double n2;
-                    double n3;
-                    while (!inputValido){
-                        try {
-                            String nomTurma = JOptionPane.showInputDialog(null, "Digite o nome da turma");
-                            String matrcl = JOptionPane.showInputDialog(null, "Digite a matricula do(a) aluno(a) que deseja alterar a nota");
-                            Aluno a1 = sistemaTurmas.buscarAlunoPorMatricula(matrcl, nomTurma);
-
-                            n1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite a primeira nota"));
-                            n2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite a segunda nota"));
-                            n3 = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite a terceira nota"));
-
-                            sistemaTurmas.alterarNotasDoAluno(a1, n1, n2, n3);
-                            JOptionPane.showMessageDialog(null, "Notas do aluno "+ a1.getNome() +" cadastradas com sucesso");
-                            inputValido = true;
-                        } catch (AlunoNaoEncontradoException | TurmaNaoEncontradaException e){
-                            JOptionPane.showMessageDialog(null, e.getMessage());
-                        } catch (NumberFormatException n) {
-                            JOptionPane.showMessageDialog(null, "Entrada inválida, digite novamente.");
-                        }
-                    }
-                    break;
-
-                case "6":
                     try {
-                        String nTurma = JOptionPane.showInputDialog(null, "Digite o nome da turma");
-                        String mat = JOptionPane.showInputDialog(null, "Digite a matricula do aluno que deseja pesquisar");
-                        Aluno alunoPesquisado = sistemaTurmas.buscarAlunoPorMatricula(mat, nTurma);
+                        nomeTurma = JOptionPane.showInputDialog("Digite o nome da turma");
+                        matricula = JOptionPane.showInputDialog("Digite a matricula do(a) aluno(a) que deseja pesquisar");
+                        Aluno alunoPesquisado = sistemaTurmas.buscarAlunoPorMatricula(matricula, nomeTurma);
                         JOptionPane.showMessageDialog(null, alunoPesquisado.toString());
                     } catch (AlunoNaoEncontradoException | TurmaNaoEncontradaException e){
                         JOptionPane.showMessageDialog(null, e.getMessage());
                     }
                     break;
 
+                case "6":
+                    try {
+                        nomeTurma = JOptionPane.showInputDialog("Digite o nome da turma que deseja remover");
+                        sistemaTurmas.removerTurma(nomeTurma);
+                        JOptionPane.showMessageDialog(null, "Turma removida com sucesso");
+                    } catch (TurmaNaoEncontradaException e){
+                        JOptionPane.showMessageDialog(null, e.getMessage());
+                    }
+
+                    break;
+
                 case "7":
                     continuar = false;
+                    try {
+                        sistemaTurmas.gravaDadosEmTurma();
+                    } catch (IOException e) {
+                        JOptionPane.showMessageDialog(null, "Não foi possível salvar os dados de Turmas.");
+                    }
                     JOptionPane.showMessageDialog(null, "Obrigado por usar o programa. Até mais!");
                     break;
 
