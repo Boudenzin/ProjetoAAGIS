af16c023e1be6350188dacdbba6c55f3e3a87ecd
gustavo-rdrgs <gustavorsantos2713@gmail.com>
2024-04-26 15:12:29 -0300

    remoção dos atributos 'notas' e 'media'

--- src/main/java/Aluno.java
+++ src/main/java/Aluno.java
@@ -2,20 +2,12 @@ import java.util.Objects;
 public class Aluno {
 
     private String nome;
-    private double media;
-    private double nota1;
-    private double nota2;
-    private double nota3;
     private String matricula;
     private String curso;
 
 
     public Aluno(String nome, String matricula, String curso) {
         this.nome = nome;
-        this.nota1 = 0.0;
-        this.nota2 = 0.0;
-        this.nota3 = 0.0;
-        this.media = calcularMedia();
         this.matricula = matricula;
         this.curso = curso;
     }
@@ -57,37 +49,11 @@ public class Aluno {
         this.curso = curso;
     }
 
-    public void setNota1(double nota1) {
-        this.nota1 = nota1;
-    }
 
-    public void setNota2(double nota2) {
-        this.nota2 = nota2;
-    }
-
-    public void setNota3(double nota3) {
-        this.nota3 = nota3;
-    }
-
-    public double getNota1() {
-        return this.nota1;
-    }
-
-    public double getNota2() {
-        return this.nota2;
-    }
-
-    public double getNota3() {
-        return this.nota3;
-    }
-
-    public double calcularMedia() {
-        return (this.nota1 + this.nota2 + this.nota3)/3;
-    }
 
     public String toString(){
         return String.format("""
-                Aluno: %s   Matricula: %s   Curso: %s   /   Nota 1: %.1f    Nota 2: %.1f    Nota3: %.1f
-                """, this.nome, this.matricula, this.curso, this.nota1, this.nota2, this.nota3);
+                Aluno: %s   Matricula: %s   Curso: %s
+                """, this.nome, this.matricula, this.curso);
     }
 }
