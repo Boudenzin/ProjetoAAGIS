bef660cd69391c89c1be8c8e7c9ffd72968aea55
Kaua-Vidal <167227277+Kaua-Vidal@users.noreply.github.com>
2024-04-26 12:42:01 -0300

    Update GravarTurmas.java

--- src/main/java/GravarTurmas.java
+++ src/main/java/GravarTurmas.java
@@ -1,5 +1,45 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+
 public class GravarTurmas {
+
+
+  
+  public List<String> recuperaTextoEmArquivo (String nomeArquivo) throws IOException{
+    BufferedReader = null;
+    List<String> textoLido = new ArrayList<>();
+    try {
+      leitor = new BufferedReader (new FileReader (nomeArquivo));
+      String texto = null;
+      do {
+        texto = readLine;
+        if(texto != null){
+          textoLido.add(texto);
+        }
+        
+      } while (texto != null);
+      
+    } finally {
+      if (leitor != null){
+        leitor.close();
+      }
+    }
+    return textoLido;
+  }
+
+
+  public void gravarTextoEmArquivo (List<String> texto, String nomeArquivo) throws IOException{
+    BufferedWriter gravador = null;
+    try {
+      gravador = new BufferedWriter (new FileWriter (nomeArquivo));
+      for (String s:texto){
+        gravador.writer(s+"\n");
+      }
+    } finally {
+      if (gravador != null){
+        gravador.close();
+      }
+    }
+  }
 }
