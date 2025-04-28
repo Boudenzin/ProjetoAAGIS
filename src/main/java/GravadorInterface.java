eacf4fcfc2d1116f12ed79f51fa1f2c9a4753046
romildo <romildo-rrj@hotmail.com>
2025-04-28 19:05:26 -0300

    feat: Cria interface para manutenabilidade

--- src/main/java/GravadorInterface.java
+++ src/main/java/GravadorInterface.java
@@ -1,2 +1,6 @@
-public class GravadorInterface {
+import java.io.IOException;
+import java.util.List;
+public interface GravadorInterface<T> {
+    void gravar(List<T> lista, String arquivo) throws IOException;
+    List<T> recuperar(String arquivo) throws IOException, ClassNotFoundException;
 }
