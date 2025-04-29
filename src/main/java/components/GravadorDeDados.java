26d3121a295f4cc18cb73103f907ac24c77bfa1f
romildo <romildo-rrj@hotmail.com>
2025-04-29 10:56:26 -0300

    refactor: Mova GravadorDeDados para diretório components

---
+++ src/main/java/components/GravadorDeDados.java
@@ -0,0 +1,27 @@
+package components;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GravadorDeDados<T> implements GravadorInterface<T> {
+
+    @Override
+    public void gravar(List<T> lista, String arquivo) throws IOException {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(arquivo))) {
+            out.writeObject(lista);
+        }
+    }
+
+    @Override
+    public List<T> recuperar(String arquivo) throws IOException, ClassNotFoundException {
+        File file = new File(arquivo);
+        if (!file.exists()) {
+            return new ArrayList<>(); // Retorna lista vazia se arquivo não existir ainda
+        }
+
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))) {
+            return (List<T>) in.readObject();
+        }
+    }
+}
