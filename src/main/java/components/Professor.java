f99e266f95b09b948192403fd2314e261a323030
romildo <romildo-rrj@hotmail.com>
2025-04-29 10:53:13 -0300

    refactor: Mova Professor para diretório components

---
+++ src/main/java/components/Professor.java
@@ -0,0 +1,31 @@
+package components;
+
+public class Professor {
+    private String nome;
+    private String matricula;
+    private String departamento;
+
+    public Professor(String nome, String matricula, String departamento) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.departamento = departamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    @Override
+    public String toString() {
+        return "components.Professor: " + nome + " | Matrícula: " + matricula + " | Departamento: " + departamento;
+    }
+}
+
