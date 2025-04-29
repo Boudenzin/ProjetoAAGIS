007ca8dd4eef03fd2524926fada8e09f7bd62659
romildo <romildo-rrj@hotmail.com>
2025-04-29 11:00:16 -0300

    feat: Crie a classe TelaCadastroTurma

---
+++ src/main/java/gui/TelaCadastroTurma.java
@@ -0,0 +1,66 @@
+package gui;
+
+import javax.swing.*;
+
+public class TelaCadastroTurma extends JFrame {
+
+    public TelaCadastroTurma() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
+        jTextField4 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Cadastro de Turma");
+
+        jLabel2.setText("Código:");
+
+        jLabel3.setText("Nome:");
+
+        jLabel4.setText("Período:");
+
+        jLabel5.setText("Professor:");
+
+        jButton1.setText("Cadastrar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                // Implementar ação do botão
+            }
+        });
+
+        // Layout configuration omitted for brevity
+
+    }
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaCadastroTurma().setVisible(true);
+            }
+        });
+    }
+
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+}
