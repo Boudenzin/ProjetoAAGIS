457a126876818b074d66e94f2ed220e97bb176f7
Romildo Júnior <romildo-rrj@hotmail.com>
2025-11-04 20:44:51 -0300

    feat: Adicione testes de remoção de aluno de TurmaTest

--- src/test/java/TurmaTest.java
+++ src/test/java/TurmaTest.java
@@ -1,4 +1,5 @@
 import exceptions.AlunoJaMatriculadoException;
+import exceptions.AlunoNaoEncontradoException;
 import model.Aluno;
 import model.AlunoTurma;
 import model.Professor;
@@ -7,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -39,6 +42,63 @@ public class TurmaTest {
         assertEquals(nomeTurma, turmaValida.getNome());
         assertNotNull(turmaValida.getParticipantes());
         assertTrue(turmaValida.getParticipantes().isEmpty());
+    }
+
+    @Test
+    @DisplayName("Deve adicionar um aluno com sucesso")
+    void deveAdicionarAlunoComSucesso() throws AlunoJaMatriculadoException {
+
+        turmaValida.adicionarAluno(alunoTeste);
+
+        assertEquals(1, turmaValida.getParticipantes().size(), "O tamanho da lista deveria ser 1");
+        assertEquals(alunoTeste, turmaValida.getParticipantes().getFirst().getAluno());
+
+
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção ao tentar adicionar o mesmo aluno duas vezes")
+    void deveLancarExcecaoAoAdicionarAlunoDuplicado() throws AlunoJaMatriculadoException {
+
+        turmaValida.adicionarAluno(alunoTeste);
+
+        assertThrows(AlunoJaMatriculadoException.class, () -> turmaValida.adicionarAluno(alunoTeste));
+
+        assertEquals(1, turmaValida.getParticipantes().size());
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma lista de participantes que não pode ser modificada")
+    void deveRetornarListaDeParticipantesNaoModificavel() {
+        //Arrange
+        List<AlunoTurma> listaDeFora = turmaValida.getParticipantes();
+        Aluno outroAluno = new Aluno("Outro", "999", "Curso", "outro", "123");
+
+        assertThrows(UnsupportedOperationException.class, () -> {
+            listaDeFora.add(new AlunoTurma(outroAluno, nomeTurma));
+        });
+    }
+
+    @Test
+    @DisplayName("Deve remover o aluno com sucesso a partir da matrícula")
+    void deveRemoverAlunoComSucesso() throws AlunoJaMatriculadoException, AlunoNaoEncontradoException {
+
+        //Arrange
+        turmaValida.adicionarAluno(alunoTeste);
+
+        //Act
+        turmaValida.removerAlunoPorMatricula(alunoTeste.getMatricula());
+
+        //Assert
+        assertEquals(0, turmaValida.getParticipantes().size());
+    }
+
+    @Test
+    @DisplayName("Deve lançar uma exceção ao remover um aluno que não existe")
+    void deveLancarExcecaoAoRemoverAlunoInexistente() throws AlunoNaoEncontradoException {
+
+        //Act & Assert
+        assertThrows(AlunoNaoEncontradoException.class, () -> turmaValida.removerAlunoPorMatricula("999"));
     }
 
 }
